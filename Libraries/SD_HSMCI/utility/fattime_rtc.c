5468dc535b332ec0b022c2dc525e6a12b61cb0e3
Jean-Marc Giacalone <jmgiacalone@hotmail.com>
2013-08-05 12:38:07 +0100

    Libraries added to project folder.

---
+++ Libraries/SD_HSMCI/utility/fattime_rtc.c
@@ -0,0 +1,91 @@
+/**
+ * \file
+ *
+ * \brief Implementation of low level disk I/O module skeleton for FatFS.
+ *
+ * Copyright (c) 2012 - 2013 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+#include "compiler.h"
+#include "rtc.h"
+
+
+uint32_t get_fattime(void);
+/**
+ * \brief Current time returned is packed into a DWORD value.
+ *
+ * The bit field is as follows:
+ *
+ * bit31:25  Year from 1980 (0..127)
+ *
+ * bit24:21  Month (1..12)
+ *
+ * bit20:16  Day in month(1..31)
+ *
+ * bit15:11  Hour (0..23)
+ *
+ * bit10:5   Minute (0..59)
+ *
+ * bit4:0    Second (0..59)
+ *
+ * \return Current time.
+ */
+uint32_t get_fattime(void)
+{
+	/**@TODO FIX THIS - real time clock required*/
+	/*
+	uint32_t ul_time;
+	uint32_t ul_hour, ul_minute, ul_second;
+	uint32_t ul_year, ul_month, ul_day, ul_week;
+
+
+	// Retrieve date and time
+	//rtc_get_time(RTC, &ul_hour, &ul_minute, &ul_second);
+	//rtc_get_date(RTC, &ul_year, &ul_month, &ul_day, &ul_week);
+
+	ul_time = ((ul_year - 1980) << 25)
+			| (ul_month << 21)
+			| (ul_day << 16)
+			| (ul_hour << 11)
+			| (ul_minute << 5)
+			| (ul_second << 0);
+
+	return ul_time;
+	*/
+	return 0x210001; //set datetime
+}
+
