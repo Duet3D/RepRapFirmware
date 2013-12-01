f0d55e0c8fb5fbaa8e9abd4388c3b31448dc31cc
Adrian Bowyer <reprappro@gmail.com>
2013-12-01 21:19:47 +0000

    README updated.

--- Configuration.h
+++ Configuration.h
@@ -24,8 +24,8 @@ Licence: GPL
 #define CONFIGURATION_H
 
 #define NAME "RepRapFirmware"
-#define VERSION "0.28"
-#define DATE "2013-11-30"
+#define VERSION "0.29"
+#define DATE "2013-12-01"
 #define LAST_AUTHOR "reprappro.com"
 
 // Other firmware that we might switch to be compatible with.
