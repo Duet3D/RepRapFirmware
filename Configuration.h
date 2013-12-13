386e3e03cdac2fd6cd3ce30797c3374b009400d3
Adrian Bowyer <reprappro@gmail.com>
2013-12-13 15:00:44 +0000

    Default steps/mm set for new pulleys.

--- Configuration.h
+++ Configuration.h
@@ -24,7 +24,7 @@ Licence: GPL
 #define CONFIGURATION_H
 
 #define NAME "RepRapFirmware"
-#define VERSION "0.37"
+#define VERSION "0.38"
 #define DATE "2013-12-13"
 #define LAST_AUTHOR "reprappro.com"
 
