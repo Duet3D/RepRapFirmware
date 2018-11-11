f000cb3b50ffac93ed9fa485fe4dc85b2c968aa9
JoergS5 <joerg.schnur@outlook.com>
2018-11-11 23:07:57 +0100

    Update FiveBarScaraKinematics.cpp

--- src/Movement/Kinematics/FiveBarScaraKinematics.cpp
+++ src/Movement/Kinematics/FiveBarScaraKinematics.cpp
@@ -30,23 +30,429 @@ const char *FiveBarScaraKinematics::GetName(bool forStatusReport) const
 
 //////////////////////// private functions /////////////////////////
 
+
 // quadrants: 1 is right upper, 2 is left upper, 3 is left down, 4 is right down
 int FiveBarScaraKinematics::getQuadrant(int x, int y) const
 {
-  return 1;
+	if(x >= 0 && y >= 0) {
+		return 1;
+	}
+	else if(x < 0 && y >= 0) {
+		return 2;
+	}
+	else if(x < 0 && y < 0) {
+		return 3;
+	}
+	else { // x >= 0 && y < 0
+		return 4;
+	}
 }
 
 
+bool FiveBarScaraKinematics::isCantilevered(int mode) const
+{
+	return true; // TODO implement
+}
 
+float FiveBarScaraKinematics::getAbsoluteAngle(float xOrig, float yOrig, float xDest, float yDest) const
+{
+	float length = sqrt(pow(xOrig - xDest, 2) + pow(yOrig - yDest,2));
+
+	float y = abs(yOrig - yDest);
+	float angle = asin(y / length) * 180.0 / M_PI;
+
+	int quad = getQuadrant(xDest-xOrig, yDest-yOrig);
+
+	if(quad == 1) {  // right upper quadrant I
+		// nothing to change
+	}
+	else if(quad == 2) {  // left upper quadrant II
+		angle = 180.0 - angle;
+	}
+	else if(quad == 3) { // left lower quadrant III
+		angle += 180.0;
+	}
+	else if(quad == 4) { // right lower quadrant IV
+		angle = 360.0 - angle;
+	}
+	return angle;
+}
+
+// first circle, second circle. Return the two intersection points
+float * FiveBarScaraKinematics::getIntersec(float firstRadius, float secondRadius, float firstX, float firstY,
+		float secondX, float secondY) const
+{
+	static float result[4];	// x,y of first point, then x,y of second
+
+	float distance = sqrt(pow((firstX - secondX),2) + pow((firstY - secondY),2));
+	float delta = 0.25 * sqrt(
+			(distance + firstRadius + secondRadius)
+			* (distance + firstRadius - secondRadius)
+			* (distance - firstRadius + secondRadius)
+			* (-distance + firstRadius + secondRadius)
+		);
+	// calculate x
+	float term1x = (firstX + secondX) / 2;
+	float term2x = (secondX - firstX)*(pow(firstRadius,2) - pow(secondRadius,2)) / (2 * pow(distance,2));
+	float term3x = 2 * (firstY - secondY) / pow(distance,2) * delta;
+	float x1 = term1x + term2x + term3x;
+	float x2 = term1x + term2x - term3x;
+
+	// calculate y
+	float term1y = (firstY + secondY) / 2;
+	float term2y = (secondY - firstY)*(pow(firstRadius,2) - pow(secondRadius,2)) / (2 * pow(distance,2));
+	float term3y = 2 * (firstX - secondX) / pow(distance,2) * delta;
+	float y1 = term1y + term2y - term3y;
+	float y2 = term1y + term2y + term3y;
+
+	result[0] = x1;
+	result[1] = y1;
+	result[2] = x2;
+	result[3] = y2;
+
+	return result;
+}
+
+
+float * FiveBarScaraKinematics::getTheta(float prox, float distal, float proxX, float proxY, float destX, float destY) const
+{
+	static float result[6];	// x,y,theta of first point, then x,y,theta of second solution
+
+	float *xyArr = getIntersec(prox, distal, proxX, proxY, destX, destY);
+	float x1 = xyArr[0];
+	float y1 = xyArr[1];
+	float x2 = xyArr[2];
+	float y2 = xyArr[3];
+
+	float thetaA = getAbsoluteAngle(proxX, proxY, x1, y1);
+	float thetaB = getAbsoluteAngle(proxX, proxY, x2, y2);
+
+	result[0] = x1;
+	result[1] = y1;
+	result[2] = thetaA;
+	result[3] = x2;
+	result[4] = y2;
+	result[5] = thetaB;
+
+	return result;
+}
+
+// from given point with angle and length, calculate destination
+float * FiveBarScaraKinematics::getXYFromAngle(float angle, float length, float origX, float origY) const
+{
+	static float result[2];	// x,y
+
+	float xL = length * cos(angle * M_PI / 180.0);
+	float yL = length * sin(angle * M_PI / 180.0);
+
+	result[0] = xL + origX;
+	result[1] = yL + origY;
+
+	return result;
+}
+
+float * FiveBarScaraKinematics::getForward(float thetaL, float thetaR, int workmode) const
+{
+	static float result[6];	// xL, yL, xR, yR, x0, y0
+
+	float *xy1 = getXYFromAngle(thetaL, proximalL, xOrigL, yOrigL);
+	float xL=xy1[0];
+	float yL=xy1[1];
+
+	xy1 = getXYFromAngle(thetaR, proximalR, xOrigR, yOrigR);
+	float xR=xy1[0];
+	float yR=xy1[1];
+
+	float *dest = getIntersec(distalL, distalR, xL, yL, xR, yR);
+
+	float x_0 = dest[0];
+	float y_0 = dest[1];
+	if(workmode == 1 || workmode == 2 || workmode == 4) {
+		if(y_0 < dest[3]) {
+			x_0 = dest[2];
+			y_0 = dest[3];
+		}
+	}
+	else {
+		// TODO error log or exception unsupported workmode
+	}
+
+	result[0] = xL;
+	result[1] = yL;
+	result[2] = xR;
+	result[3] = yR;
+	result[4] = x_0;
+	result[5] = y_0;
+	return result;
+}
+
+// 1 - angle - 2 are ordered clockwise. The angle is at the inner/right side, between 2 and 1 clockwise
+float FiveBarScaraKinematics::getAngle(float x1, float y1, float xAngle, float yAngle, float x2, float y2) const
+{
+	float angle1 = getAbsoluteAngle(xAngle, yAngle, x1, y1);
+	float angle2 = getAbsoluteAngle(xAngle, yAngle, x2, y2);
+
+	float angle = 0.0;
+	if(angle2 < angle1) {
+		angle = 360 + angle2 - angle1;
+	}
+	else {
+		angle = angle2 - angle1;
+	}
+
+	return angle;
+}
+
+// test angle to be between min and max value and return true or false
+bool FiveBarScaraKinematics::constraintOk(float angleMin, float angleMax, float angle) const
+{
+	if(angle >= angleMin && angle <= angleMax) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 
 
 ///////////////////////////// public functions /////////////////////////
 
+
+// Set the parameters from a M665, M666 or M669 command
+// Return true if we changed any parameters. Set 'error' true if there was an error, otherwise leave it alone.
+bool FiveBarScaraKinematics::Configure(unsigned int mCode, GCodeBuffer& gb, const StringRef& reply, bool& error) /*override*/
+{
+	if (mCode == 669)
+	{
+		bool seen = false;
+		bool seenNonGeometry = false;
+
+		// parameter X: x values of actuators
+		float paraX[2];
+		if (gb.TryGetFloatArray('X', 2, paraX, reply, seen))
+		{
+			error = true;
+			return true;
+		}
+		xOrigL = paraX[0];
+		xOrigR = paraX[1];
+
+		// parameter Y: y values of actuators
+		float paraY[2];
+		if (gb.TryGetFloatArray('Y', 2, paraY, reply, seen))
+		{
+			error = true;
+			return true;
+		}
+		yOrigL = paraY[0];
+		yOrigR = paraY[1];
+
+
+
+
+		/*
+		gb.TryGetFValue('P', proximalArmLength, seen);
+		gb.TryGetFValue('D', distalArmLength, seen);
+		gb.TryGetFValue('S', segmentsPerSecond, seenNonGeometry);
+		gb.TryGetFValue('T', minSegmentLength, seenNonGeometry);
+		gb.TryGetFValue('X', xOffset, seen);
+		gb.TryGetFValue('Y', yOffset, seen);
+		if (gb.TryGetFloatArray('A', 2, thetaLimits, reply, seen))
+		{
+			error = true;
+			return true;
+		}
+		if (gb.TryGetFloatArray('B', 2, psiLimits, reply, seen))
+		{
+			error = true;
+			return true;
+		}
+		if (gb.TryGetFloatArray('C', 3, crosstalk, reply, seen))
+		{
+			error = true;
+			return true;
+		}
+		*/
+
+		if (seen || seenNonGeometry)
+		{
+			Recalc();
+		}
+		else if (!gb.Seen('K'))
+		{
+			/*
+			reply.printf("Kinematics is Scara with proximal arm %.2fmm range %.1f to %.1f" DEGREE_SYMBOL
+							"%s, distal arm %.2fmm range %.1f to %.1f" DEGREE_SYMBOL "%s, crosstalk %.1f:%.1f:%.1f, bed origin (%.1f, %.1f), segments/sec %d, min. segment length %.2f",
+							(double)proximalArmLength, (double)thetaLimits[0], (double)thetaLimits[1], (supportsContinuousRotation[0]) ? " (continuous)" : "",
+							(double)distalArmLength, (double)psiLimits[0], (double)psiLimits[1], (supportsContinuousRotation[0]) ? " (continuous)" : "",
+							(double)crosstalk[0], (double)crosstalk[1], (double)crosstalk[2],
+							(double)xOffset, (double)yOffset,
+							(int)segmentsPerSecond, (double)minSegmentLength);
+							*/
+		}
+		return seen;
+	}
+	else
+	{
+		return ZLeadscrewKinematics::Configure(mCode, gb, reply, error);
+	}
+}
+
+
 // Convert Cartesian coordinates to motor coordinates, returning true if successful
 // In the following, theta is the proximal arm angle relative to the X axis, psi is the distal arm angle relative to the proximal arm
 bool FiveBarScaraKinematics::CartesianToMotorSteps(const float machinePos[], const float stepsPerMm[], size_t numVisibleAxes, size_t numTotalAxes, int32_t motorPos[], bool isCoordinated) const
 {
+	float x_0 = machinePos[0];
+	float y_0 = machinePos[1];
+
+	float thetaL = -1.0;
+	float thetaR = -1.0;
+
+	if(isCantilevered(1)) {
+		// calculate cantilevered side first:
+		float *theta = getTheta(proximalL, distalL + cantL, xOrigL, yOrigL, x_0, y_0);
+		float xL = theta[0];
+		float yL = theta[1];
+		thetaL = theta[2];
+
+		if(workmode == 1) {
+			if(theta[0] < theta[3]) {
+				xL = theta[3];
+				yL = theta[4];
+				thetaL = theta[5];
+			}
+		}
+		else if(workmode == 2 || workmode == 4) {
+			if(theta[0] > theta[3]) {
+				xL = theta[3];
+				yL = theta[4];
+				thetaL = theta[5];
+			}
+		}
+		else {
+			// unsupported workmode => TODO error log or exception
+		}
+
+		// calculate x1,y1, i.e. where the distal arms meet
+		float fraction = distalL / (distalL + cantL);
+		float x1 = (x_0 - xL) * fraction + xL;
+		float y1 = (y_0 - yL) * fraction + yL;
+
+		// calculate right, non cantilevered side:
+		theta = getTheta(proximalR, distalR, xOrigR, yOrigR, x1, y1);
+		float xR = theta[0];
+		float yR = theta[1];
+		thetaR = theta[2];
+
+		if(workmode == 1 || workmode == 2) {
+			if(theta[0] < theta[3]) {
+				xR = theta[3];
+				yR = theta[4];
+				thetaR = theta[5];
+			}
+		}
+		else if(workmode == 4) {
+			if(theta[0] > theta[3]) {
+				xR = theta[3];
+				yR = theta[4];
+				thetaR = theta[5];
+			}
+		}
+		else {
+			// unsupported workmode => TODO error log or exception
+		}
+	}
+	else if(isCantilevered(2)) {
+		// calculate cantilevered side first:
+		float *theta = getTheta(proximalR, distalR + cantR, xOrigR, yOrigR, x_0, y_0);
+		float xR = theta[0];
+		float yR = theta[1];
+		thetaR = theta[2];
+
+		if(workmode == 1 || workmode == 2) {
+			if(theta[0] < theta[3]) {
+				xR = theta[3];
+				yR = theta[4];
+				thetaR = theta[5];
+			}
+		}
+		else if(workmode == 4) {
+			if(theta[0] > theta[3]) {
+				xR = theta[3];
+				yR = theta[4];
+				thetaR = theta[5];
+			}
+		}
+		else {
+			// unsupported workmode => TODO error log or exception
+		}
+
+		// calculate x1,y1, i.e. where the distal arms meet
+		float fraction = distalR / (distalR + cantR);
+		float x1 = (x_0 - xR) * fraction + xR;
+		float y1 = (y_0 - yR) * fraction + yR;
+
+		// calculate left, non cantilevered side:
+		theta = getTheta(proximalL, distalL, xOrigL, yOrigL, x1, y1);
+		float xL = theta[0];
+		float yL = theta[1];
+		thetaL = theta[2];
+
+		if(workmode == 1) {
+			if(theta[0] < theta[3]) {
+				xL = theta[3];
+				yL = theta[4];
+				thetaL = theta[5];
+			}
+		}
+		else if(workmode == 2 || workmode == 4) {
+			if(theta[0] > theta[3]) {
+				xL = theta[3];
+				yL = theta[4];
+				thetaL = theta[5];
+			}
+		}
+		else {
+			// unsupported workmode => TODO error log or exception
+		}
+	}
+	else {	// not cantilevered, hotend is at top joint
+		float *theta = getTheta(proximalL, distalL, xOrigL, yOrigL, x_0, y_0);
+		float thetaL1 = theta[2];
+		float thetaL2 = theta[5];
+		if(workmode == 1) {
+			if(thetaL1 <= thetaL2) {
+				thetaL = thetaL1;
+			}
+			else {
+				thetaL = thetaL2;
+			}
+		}
+
+		theta = getTheta(proximalR, distalR, xOrigR, yOrigR, x_0, y_0);
+		float thetaR1 = theta[2];
+		float thetaR2 = theta[5];
+		if(workmode == 1) {
+			if(thetaR1 <= thetaR2) {
+				thetaR = thetaR1;
+			}
+			else {
+				thetaR = thetaR2;
+			}
+		}
+	}
+
+	motorPos[X_AXIS] = lrintf(thetaL * stepsPerMm[X_AXIS]);
+	motorPos[Y_AXIS] = lrintf(thetaR * stepsPerMm[Y_AXIS]);
+	motorPos[Z_AXIS] = lrintf(machinePos[Z_AXIS] * stepsPerMm[Z_AXIS]);
+
+	// Transform any additional axes linearly
+	for (size_t axis = XYZ_AXES; axis < numVisibleAxes; ++axis)
+	{
+		motorPos[axis] = lrintf(machinePos[axis] * stepsPerMm[axis]);
+	}
 	return true;
 }
 
@@ -54,6 +460,52 @@ bool FiveBarScaraKinematics::CartesianToMotorSteps(const float machinePos[], con
 // For Scara, the X and Y components of stepsPerMm are actually steps per degree angle.
 void FiveBarScaraKinematics::MotorStepsToCartesian(const int32_t motorPos[], const float stepsPerMm[], size_t numVisibleAxes, size_t numTotalAxes, float machinePos[]) const
 {
+	float thetaL = ((float)motorPos[X_AXIS]/stepsPerMm[X_AXIS]);
+	float thetaR = ((float)motorPos[Y_AXIS]/stepsPerMm[Y_AXIS]);
+
+	float x_0 = -1.0;
+	float y_0 = -1.0;
+
+	float *result = getForward(thetaL, thetaR, workmode);
+	float x1 = result[4];
+	float y1 = result[5];
+
+	if(isCantilevered(1)) {	// left distal is prolongued
+		float xL = result[0];
+		float yL = result[1];
+
+		// calculate cantilever from psiL
+		float psiL = getAbsoluteAngle(xL, yL, x1, y1);
+		float *destXY = getXYFromAngle(psiL, cantL, x1, y1);
+		x_0 = destXY[0];
+		y_0 = destXY[1];
+	}
+	else if(isCantilevered(2)) { // right distal is prolongued
+		float xR = result[2];
+		float yR = result[3];
+
+		// now calculate cantilever from psiR
+		float psiR = getAbsoluteAngle(xR, yR, x1, y1);
+		float *destXY = getXYFromAngle(psiR, cantR, x1, y1);
+		x_0 = destXY[0];
+		y_0 = destXY[1];
+	}
+	else {
+		x_0 = x1;
+		y_0 = y1;
+	}
+
+
+    machinePos[X_AXIS] = x_0;
+    machinePos[Y_AXIS] = y_0;
+
+    machinePos[Z_AXIS] = (float) motorPos[Z_AXIS] / stepsPerMm[Z_AXIS];
+
+	// Convert any additional axes linearly
+	for (size_t drive = XYZ_AXES; drive < numVisibleAxes; ++drive)
+	{
+		machinePos[drive] = motorPos[drive]/stepsPerMm[drive];
+	}
 }
 
 // Return true if the specified XY position is reachable by the print head reference point.
