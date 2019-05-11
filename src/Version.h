7684fee498303fe1803966538be1935e5685ad2d
David Crocker <dcrocker@eschertech.com>
2019-05-11 13:02:15 +0100

    Update Version.h
    Increased version to 2.03RC1+1/1.24RC1+1 because CoreNG has changed (bug fix for SAM3X8E builds).

--- src/Version.h
+++ src/Version.h
@@ -11,16 +11,16 @@
 
 #ifndef VERSION
 #ifdef RTOS
-# define MAIN_VERSION	"2.03RC1"
+# define MAIN_VERSION	"2.03RC1+1"
 #else
-# define MAIN_VERSION	"1.24RC1"
+# define MAIN_VERSION	"1.24RC1+1"
 #endif
 
 # define VERSION MAIN_VERSION
 #endif
 
 #ifndef DATE
-# define DATE "2019-05-08b1"
+# define DATE "2019-05-11b1"
 #endif
 
 #define AUTHORS "reprappro, dc42, chrishamm, t3p3, dnewman, printm3d"
