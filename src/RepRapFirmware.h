980b356737e65a8966b6372f2f16c6ebde0419e3
David Crocker <dcrocker@eschertech.com>
2019-11-01 12:08:43 +0000

    Still 2.04 provisional
    Corrected project references
    Fixed RepRapFirmware.h for compatibility with new CMSIS version

--- src/RepRapFirmware.h
+++ src/RepRapFirmware.h
@@ -178,15 +178,6 @@ void ListDrivers(const StringRef& str, DriversBitmap drivers);
 
 // Functions to change the base priority, to shut out interrupts up to a priority level
 
-// From section 3.12.7 of http://infocenter.arm.com/help/topic/com.arm.doc.dui0553b/DUI0553.pdf:
-// When you write to BASEPRI_MAX, the instruction writes to BASEPRI only if either:
-// � Rn is non-zero and the current BASEPRI value is 0
-// � Rn is non-zero and less than the current BASEPRI value
-__attribute__( ( always_inline ) ) __STATIC_INLINE void __set_BASEPRI_MAX(uint32_t value)
-{
-  __ASM volatile ("MSR basepri_max, %0" : : "r" (value) : "memory");
-}
-
 // Get the base priority and shut out interrupts lower than or equal to a specified priority
 inline uint32_t ChangeBasePriority(uint32_t prio)
 {
