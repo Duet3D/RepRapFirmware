e82f7335bcb12e03e0bd7fc9faac2bb409714688
Manuel Coenen <manuel.coenen@gmail.com>
2020-05-12 16:06:15 +0200

    Remove usage of later-defined macros

--- src/DuetNG/Pins_DuetNG.h
+++ src/DuetNG/Pins_DuetNG.h
@@ -42,9 +42,9 @@ constexpr size_t NumFirmwareUpdateModules = 4;		// 3 modules, plus one for manua
 #define SUPPORT_WORKPLACE_COORDINATES	1			// set nonzero to support G10 L2 and G53..59
 #define SUPPORT_12864_LCD		0					// set nonzero to support 12864 LCD and rotary encoder
 #define SUPPORT_OBJECT_MODEL	1
-#define SUPPORT_HTTP			(1 && HAS_NETWORKING)
-#define SUPPORT_FTP				(1 && HAS_NETWORKING)
-#define SUPPORT_TELNET			(1 && HAS_NETWORKING)
+#define SUPPORT_HTTP			1
+#define SUPPORT_FTP				1
+#define SUPPORT_TELNET			1
 #define SUPPORT_ASYNC_MOVES		1
 #define ALLOCATE_DEFAULT_PORTS	0
 #define TRACK_OBJECT_NAMES		1
