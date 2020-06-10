3d407b9a39fb7fcb9ee17d097fec869b4c001b6a
David Crocker <dcrocker@eschertech.com>
2020-06-10 11:57:35 +0100

    Renamed Duet3Limits.h to Duet3Common.h

--- src/Duet3_V06/Pins_Duet3_V06.h
+++ src/Duet3_V06/Pins_Duet3_V06.h
@@ -52,7 +52,7 @@ constexpr uint32_t IAP_IMAGE_START = 0x20450000;		// last 64kb of RAM
 
 // The physical capabilities of the machine
 
-#include <Duet3Limits.h>							// this file is in the CANlib project because both main and expansion boards need it
+#include <Duet3Common.h>							// this file is in the CANlib project because both main and expansion boards need it
 
 constexpr size_t NumDirectDrivers = 6;				// The maximum number of drives supported by the electronics inc. direct expansion
 constexpr size_t MaxSmartDrivers = 6;				// The maximum number of direct smart drivers
