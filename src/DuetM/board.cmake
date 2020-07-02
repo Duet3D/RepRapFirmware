7bbc354ba4777a5a9e60761d04b449e4f7416cdc
likha3d <dev@likha3d.com>
2020-07-02 15:42:45 +0800

    Add instead of remove networking files

--- src/DuetM/board.cmake
+++ src/DuetM/board.cmake
@@ -4,12 +4,15 @@ add_link_options("-mcpu=cortex-m4")
 
 list(APPEND SRCS
     "${CMAKE_CURRENT_LIST_DIR}/Pins_DuetM.cpp"
-)
 
-list(REMOVE_ITEM SRCS
-    "src/Networking/ESP8266WiFi/WiFiInterface.cpp"
-    "src/Networking/ESP8266WiFi/WiFiSocket.cpp"
-    "src/Networking/ESP8266WiFi/WifiFirmwareUploader.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/Wiznet/Internet/DHCP/dhcp.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/Wiznet/Ethernet/W5500/w5500.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/Wiznet/Ethernet/WizSpi.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/Wiznet/Ethernet/socketlib.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/Wiznet/Ethernet/wizchip_conf.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/MdnsResponder.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/W5500Interface.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/W5500Socket.cpp"
 )
 
 list(APPEND INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}")
--- src/DuetNG/board.cmake
+++ src/DuetNG/board.cmake
@@ -6,6 +6,19 @@ list(APPEND SRCS
     "${CMAKE_CURRENT_LIST_DIR}/DueXn.cpp"
     "${CMAKE_CURRENT_LIST_DIR}/Pins_DuetNG.cpp"
     "${CMAKE_CURRENT_LIST_DIR}/SX1509.cpp"
+
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/ESP8266WiFi/WiFiInterface.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/ESP8266WiFi/WiFiSocket.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/ESP8266WiFi/WifiFirmwareUploader.cpp"
+
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/Wiznet/Internet/DHCP/dhcp.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/Wiznet/Ethernet/W5500/w5500.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/Wiznet/Ethernet/WizSpi.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/Wiznet/Ethernet/socketlib.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/Wiznet/Ethernet/wizchip_conf.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/MdnsResponder.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/W5500Interface.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Networking/W5500Ethernet/W5500Socket.cpp"
 )
 list(APPEND INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}")
 
