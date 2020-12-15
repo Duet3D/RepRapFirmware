53d77c35319672bff60dd39df7be5ea03bbe89d2
Christian Hammacher <bmasterc@gmail.com>
2020-12-15 18:33:23 +0100

    Fixed two more bugs in the SBC interface

--- src/Linux/LinuxInterface.cpp
+++ src/Linux/LinuxInterface.cpp
@@ -460,9 +460,6 @@ void LinuxInterface::Init() noexcept
 						{
 							filament->Load(filamentName.c_str());
 						}
-
-						TaskCriticalSectionLocker locker;
-						reprap.MoveUpdated();
 					}
 					break;
 				}
@@ -859,6 +856,11 @@ bool LinuxInterface::FillBuffer(GCodeBuffer &gb) noexcept
 			uint16_t readPointer = rxPointer;
 			do
 			{
+				if (readPointer == txLength)
+				{
+					readPointer = 0;
+				}
+
 				BufferedCodeHeader * const bufHeader = reinterpret_cast<BufferedCodeHeader*>(codeBuffer + readPointer);
 				readPointer += sizeof(BufferedCodeHeader);
 				const CodeHeader * const header = reinterpret_cast<const CodeHeader*>(codeBuffer + readPointer);
@@ -913,15 +915,7 @@ bool LinuxInterface::FillBuffer(GCodeBuffer &gb) noexcept
 						gotCommand = true;
 						break;
 					}
-					else
-					{
-						updateRxPointer = false;
-					}
-				}
-
-				if (readPointer == txLength)
-				{
-					readPointer = 0;
+					updateRxPointer = false;
 				}
 			} while (readPointer != txPointer);
 		}
