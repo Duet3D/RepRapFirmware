9973e73a060a3ee0395367d57e9a0c4ab3609b99
David Crocker <dcrocker@eschertech.com>
2021-08-16 17:08:33 +0100

    Fixed issues caused by failed merge

--- src/GCodes/GCodes.cpp
+++ src/GCodes/GCodes.cpp
@@ -2784,7 +2784,7 @@ bool GCodes::DoFileMacro(GCodeBuffer& gb, const char* fileName, bool reportMissi
 	else
 #endif
 	{
-#if HAS_MASS_STORAGE || HAS_EMBEDDED_FILES || HAS_EMBEDDED_FILES
+#if HAS_MASS_STORAGE || HAS_EMBEDDED_FILES
 		FileStore * const f = platform.OpenSysFile(fileName, OpenMode::read);
 		if (f == nullptr)
 		{
