309908915d87b959590710c045207f53c68ea8f9
David Crocker <dcrocker@eschertech.com>
2021-10-06 12:04:04 +0100

    Bug fix: default MAC address did not use last byte of the MCU unique ID

--- src/Platform/UniqueId.cpp
+++ src/Platform/UniqueId.cpp
@@ -17,10 +17,11 @@ void UniqueId::AppendCharsToBuffer(OutputBuffer *buf) const noexcept
 void UniqueId::GenerateMacAddress(MacAddress& addr) const noexcept
 {
 	// The unique ID is 128 bits long whereas the whole MAC address is only 48 bits, so we can't guarantee that each Duet will get a unique MAC address this way.
+	// There are 16 ID bytes not including the checksum. It appears that the last bytes are more likely to change, so they must be included.
 	memset(addr.bytes, 0, sizeof(addr.bytes));
 	addr.bytes[0] = 0xBE;					// use a fixed first byte with the locally-administered bit set
 	const uint8_t * const idBytes = reinterpret_cast<const uint8_t *>(data);
-	for (size_t i = 0; i < 15; ++i)
+	for (size_t i = 0; i < 16; ++i)
 	{
 		addr.bytes[(i % 5) + 1] ^= idBytes[i];
 	}
