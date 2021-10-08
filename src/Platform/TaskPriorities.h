b531515e748b8f0c70c5429586994230aa1aab34
David Crocker <dcrocker@eschertech.com>
2021-10-08 22:51:36 +0100

    Improved CAN receiver task priority, for closed loop data collection

--- src/Platform/TaskPriorities.h
+++ src/Platform/TaskPriorities.h
@@ -11,30 +11,29 @@
 // Task priorities
 namespace TaskPriority
 {
-	constexpr int IdlePriority = 0;
-	constexpr int SpinPriority = 1;							// priority for tasks that rarely block
+	constexpr unsigned int IdlePriority = 0;
+	constexpr unsigned int SpinPriority = 1;						// priority for tasks that rarely block
 #if HAS_LINUX_INTERFACE
-	constexpr int SbcPriority = 2;							// priority for SBC task
+	constexpr unsigned int SbcPriority = 2;							// priority for SBC task
 #endif
 #if defined(LPC_NETWORKING)
     constexpr int TcpPriority  = 2;
     //EMAC priority = 3 defined in FreeRTOSIPConfig.h
 #endif
-    constexpr int HeatPriority = 3;
-	constexpr int Move = 4;
-	constexpr int SensorsPriority = 4;
-	constexpr int TmcPriority = 4;
-	constexpr int AinPriority = 4;
-	constexpr int HeightFollowingPriority = 4;
+    constexpr unsigned int HeatPriority = 3;
+	constexpr unsigned int Move = 4;
+	constexpr unsigned int TmcPriority = 4;
+	constexpr unsigned int AinPriority = 4;
+	constexpr unsigned int HeightFollowingPriority = 4;
 #ifdef DUET_NG
-	constexpr int DueXPriority = 5;
+	constexpr unsigned int DueXPriority = 5;
 #endif
-	constexpr int LaserPriority = 5;
-	constexpr int CanSenderPriority = 5;
-	constexpr int CanReceiverPriority = 5;
-	constexpr int EthernetPriority = 5;
-	constexpr int Accelerometer = 6;
-	constexpr int CanClockPriority = 7;
+	constexpr unsigned int LaserPriority = 5;
+	constexpr unsigned int CanSenderPriority = 5;
+	constexpr unsigned int EthernetPriority = 5;
+	constexpr unsigned int CanReceiverPriority = 6;
+	constexpr unsigned int Accelerometer = 6;
+	constexpr unsigned int CanClockPriority = 7;
 }
 
 #endif /* SRC_TASKPRIORITIES_H_ */
