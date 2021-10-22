89e64be0cb6b8fb9f39a5fac0c47fc8d91ce137d
David Crocker <dcrocker@eschertech.com>
2021-10-22 21:33:15 +0100

    Moved some constants from Configuration.h to RRF3Common.h

--- src/Configuration.h
+++ src/Configuration.h
@@ -2,10 +2,6 @@
 
 RepRapFirmware - Configuration
 
-This is where all machine-independent configuration and other definitions are set up. Nothing that
-depends on any particular RepRap, RepRap component, or RepRap controller should go in here. Define
-machine-dependent things in Platform.h
-
 -----------------------------------------------------------------------------------------------------
 
 Version 0.1
@@ -26,11 +22,6 @@ Licence: GPL
 #include <cstddef>			// for size_t
 #include <cstring>			// for strlen
 
-// Generic constants
-constexpr float ABS_ZERO = -273.15;						// Celsius
-constexpr float NEARLY_ABS_ZERO = -273.0;				// Celsius
-constexpr float ROOM_TEMPERATURE = 21.0;				// Celsius
-
 // Axes
 constexpr float DefaultXYMaxFeedrate = 100;				// mm/sec
 constexpr float DefaultZMaxFeedrate = 5.0;
@@ -39,6 +30,9 @@ constexpr float DefaultEMaxFeedrate = 20.0;
 constexpr float DefaultXYAcceleration = 500.0;			// mm/sec^2
 constexpr float DefaultZAcceleration = 20.0;
 constexpr float DefaultEAcceleration = 250.0;
+
+constexpr float XYStallEndstopMaxAcceleration = 1000.0;	// Maximum XY acceleration to use at the start of a homing move if stall endstops are being used
+constexpr float ZProbeMaxAcceleration = 250.0;			// Maximum Z acceleration to use at the start of a probing or stall endstop move
 
 constexpr float DefaultXYDriveStepsPerUnit = 80.0;		// steps/mm
 constexpr float DefaultZDriveStepsPerUnit = 4000.0;
@@ -58,22 +52,13 @@ constexpr float DefaultTravelAcceleration = 20000.0;	// higher than the likely m
 constexpr float DefaultAxisMinimum = 0.0;
 constexpr float DefaultAxisMaximum = 200.0;
 
-constexpr uint32_t MaxTools = 50;						// this limit is to stop the serialised object model getting too large
+constexpr unsigned int MaxTools = 50;					// this limit is to stop the serialised object model getting too large
 constexpr unsigned int MinVisibleAxes = 2;				// the minimum number of axes that we allow to be visible
 
 // Timeouts
-constexpr uint32_t FanCheckInterval = 500;				// Milliseconds
-constexpr uint32_t OpenLoadTimeout = 500;				// Milliseconds
-constexpr uint32_t MinimumWarningInterval = 4000;		// Milliseconds, must be at least as long as FanCheckInterval
 constexpr uint32_t LogFlushInterval = 15000;			// Milliseconds
-constexpr uint32_t DriverCoolingTimeout = 4000;			// Milliseconds
 constexpr float DefaultMessageTimeout = 10.0;			// How long a message is displayed by default, in seconds
 constexpr uint16_t MinimumGpinReportInterval = 30;		// Minimum interval in milliseconds between input change reports sent over CAN bus
-
-constexpr uint32_t MinimumOpenLoadFullStepsPerSec = 20;	// this is 4mm/sec @ 80steps/mm
-
-// FanCheckInterval must be lower than MinimumWarningInterval to avoid giving driver over temperature warnings too soon when thermostatic control of electronics cooling fans is used
-static_assert(FanCheckInterval < MinimumWarningInterval, "FanCheckInterval too large");
 
 // Comms defaults
 constexpr unsigned int MAIN_BAUD_RATE = 115200;			// Default communication speed of the USB if needed
@@ -81,58 +66,8 @@ constexpr unsigned int AUX_BAUD_RATE = 57600;			// Ditto - for auxiliary UART de
 constexpr unsigned int AUX2_BAUD_RATE = 115200;			// Ditto - for second auxiliary UART device
 constexpr uint32_t SERIAL_MAIN_TIMEOUT = 2000;			// timeout in ms for sending data to the main serial/USB port
 constexpr uint32_t AuxTimeout = 2000;					// timeout in ms for PanelDue replies
+
 #define PANEL_DUE_FIRMWARE_FILE "PanelDueFirmware.bin"
-
-// Heater values
-constexpr uint32_t HeatSampleIntervalMillis = 250;		// interval between taking temperature samples
-constexpr float HeatPwmAverageTime = 5.0;				// Seconds
-
-constexpr uint8_t SensorsTaskTotalDelay = 250;			// Interval between runs of sensors task
-
-constexpr float TEMPERATURE_CLOSE_ENOUGH = 1.0;			// Celsius
-constexpr float TEMPERATURE_LOW_SO_DONT_CARE = 40.0;	// Celsius
-constexpr float HOT_ENOUGH_TO_EXTRUDE = 160.0;			// Celsius
-constexpr float HOT_ENOUGH_TO_RETRACT = 90.0;			// Celsius
-
-constexpr unsigned int MaxBadTemperatureCount = 2000/HeatSampleIntervalMillis;	// Number of bad temperature samples permitted before a heater fault is reported (2 seconds)
-constexpr float BadLowTemperature = -10.0;				// Celsius
-constexpr float DefaultHotEndTemperatureLimit = 285.0;	// Celsius - E3D say to tighten the hot end at 285C
-constexpr float DefaultBedTemperatureLimit = 125.0;		// Celsius
-constexpr float DefaultAllowedOverTemperature = 5.0;
-constexpr float DefaultHotEndFanTemperature = 45.0;		// Temperature at which a thermostatic hot end fan comes on
-constexpr float ThermostatHysteresis = 1.0;				// How much hysteresis we use to prevent noise turning fans on/off too often
-constexpr float BadErrorTemperature = 2000.0;			// Must exceed any reasonable temperature limit including DEFAULT_TEMPERATURE_LIMIT
-constexpr uint32_t DefaultHeaterFaultTimeout = 10 * 60 * 1000;	// How long we wait (in milliseconds) for user intervention after a heater fault before shutting down
-
-// Heating model default parameters. For the chamber heater, we use the same values as for the bed heater.
-// These parameters are about right for an E3Dv6 hot end with 30W heater.
-constexpr float DefaultHotEndHeaterCoolingRate = 1.0/140.0;		// E3D V6 has a cooling time constant of about 140 seconds with the fan off
-constexpr float DefaultHotEndHeaterHeatingRate = 340.0 * DefaultHotEndHeaterCoolingRate;
-constexpr float DefaultHotEndHeaterDeadTime = 5.5;		// E3D v6
-
-// These parameters are about right for a typical PCB bed heater that maxes out at 110C
-constexpr float DefaultBedHeaterCoolingRate = 1.0/700.0;
-constexpr float DefaultBedHeaterHeatingRate = 90.0 * DefaultBedHeaterCoolingRate;
-constexpr float DefaultBedHeaterDeadTime = 10.0;
-
-// Parameters used to detect heating errors
-constexpr float DefaultMaxHeatingFaultTime = 5.0;		// How many seconds we allow a heating fault to persist
-constexpr float AllowedTemperatureDerivativeNoise = 0.12;	// How much fluctuation in the averaged temperature derivative we allow
-constexpr float MaxAmbientTemperature = 45.0;			// We expect heaters to cool to this temperature or lower when switched off
-constexpr float NormalAmbientTemperature = 25.0;		// The ambient temperature we assume - allow for the printer heating its surroundings a little
-constexpr float LowAmbientTemperature = 15.0;			// A lower ambient temperature that we assume when checking heater performance
-constexpr float DefaultMaxTempExcursion = 15.0;			// How much error we tolerate when maintaining temperature before deciding that a heater fault has occurred
-constexpr float MinimumConnectedTemperature = -5.0;		// Temperatures below this we treat as a disconnected thermistor
-
-static_assert(DefaultMaxTempExcursion > TEMPERATURE_CLOSE_ENOUGH, "DefaultMaxTempExcursion is too low");
-
-// PWM frequencies
-constexpr PwmFrequency SlowHeaterPwmFreq = 10;			// slow PWM frequency for bed and chamber heaters, compatible with DC/AC SSRs
-constexpr PwmFrequency DefaultHeaterPwmFreq = 250;		// normal PWM frequency used for hot ends
-constexpr PwmFrequency MaxHeaterPwmFrequency = 1000;	// maximum supported heater PWM frequency, to avoid overheating the mosfets
-constexpr PwmFrequency DefaultFanPwmFreq = 250;			// increase to 25kHz using M106 command to meet Intel 4-wire PWM fan specification
-constexpr PwmFrequency DefaultPinWritePwmFreq = 500;	// default PWM frequency for M42 pin writes and extrusion ancillary PWM
-constexpr PwmFrequency ServoRefreshFrequency = 50;
 
 // Conditional GCode support
 constexpr unsigned int MaxBlockIndent = 10;				// maximum indentation of GCode. Each level of indentation introduced a new block.
@@ -186,13 +121,11 @@ constexpr float DefaultZProbeTemperature = 25.0;
 constexpr float DefaultZDive = 5.0;						// Millimetres
 constexpr float DefaultProbingSpeed = 2.0;				// Default Z probing speed mm/sec
 constexpr float DefaultZProbeTravelSpeed = 100.0;		// Default speed for travel to probe points
-constexpr float ZProbeMaxAcceleration = 250.0;			// Maximum Z acceleration to use at the start of a probing move
 
 #if !defined(DUET3) && !defined(DUET3MINI)				// for Duet 3 these are defined in Duet3Common.h in project CANLib
 constexpr size_t MaxZProbeProgramBytes = 8;				// Maximum number of bytes in a Z probe program
 #endif
 
-constexpr uint32_t ProbingSpeedReductionFactor = 3;		// The factor by which we reduce the Z probing speed when we get a 'near' indication
 constexpr float DefaultZProbeTolerance = 0.03;			// How close the Z probe trigger height from consecutive taps must be
 constexpr uint8_t DefaultZProbeTaps = 1;				// The maximum number of times we probe each point
 constexpr int DefaultZProbeADValue = 500;				// Default trigger threshold
@@ -346,15 +279,15 @@ constexpr size_t FILE_BUFFER_SIZE = 128;
 #define FOUR04_PAGE_FILE		"html404.htm"
 
 // Filesystem and upload defaults
-#define FS_PREFIX "0:"
-#define WEB_DIR "0:/www/"							// Place to find web files on the SD card
-#define GCODE_DIR "0:/gcodes/"						// Ditto - G-Codes
-#define DEFAULT_SYS_DIR "0:/sys/"					// Ditto - System files (can be changed using M505)
-#define MACRO_DIR "0:/macros/"						// Ditto - Macro files
-#define SCANS_DIRECTORY "0:/scans/"					// Directory for uploaded 3D scans
-#define FILAMENTS_DIRECTORY "0:/filaments/"			// Directory for filament configurations
-#define FIRMWARE_DIRECTORY "0:/firmware/"			// Directory for firmware and IAP files
-#define MENU_DIR "0:/menu/"							// Directory for menu files
+#define FS_PREFIX				"0:"
+#define WEB_DIR					"0:/www/"				// Place to find web files on the SD card
+#define GCODE_DIR				"0:/gcodes/"			// Ditto - G-Codes
+#define DEFAULT_SYS_DIR			"0:/sys/"				// Ditto - System files (can be changed using M505)
+#define MACRO_DIR				"0:/macros/"			// Ditto - Macro files
+#define SCANS_DIRECTORY			"0:/scans/"				// Directory for uploaded 3D scans
+#define FILAMENTS_DIRECTORY		"0:/filaments/"			// Directory for filament configurations
+#define FIRMWARE_DIRECTORY		"0:/firmware/"			// Directory for firmware and IAP files
+#define MENU_DIR				"0:/menu/"				// Directory for menu files
 
 // MaxExpectedWebDirFilenameLength is the maximum length of a filename that we can accept in a HTTP request without rejecting it out of hand
 // and perhaps warning the user of a possible virus attack.
