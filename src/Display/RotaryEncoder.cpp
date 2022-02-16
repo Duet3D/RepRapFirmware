e796fd87c6a6bab5a343fb3f88449779aa7ceb41
David Crocker <dcrocker@eschertech.com>
2022-02-16 16:17:26 +0000

    Changed rotary encoder transition table to be less noise-sensitive

--- src/Display/RotaryEncoder.cpp
+++ src/Display/RotaryEncoder.cpp
@@ -39,20 +39,20 @@ void RotaryEncoder::Poll() noexcept
 	// 0 - the encoder hasn't moved
 	// 1 or 2 - the encoder has moved 1 or 2 units clockwise
 	// -1 or -2 = the encoder has moved 1 or 2 units anticlockwise
-	static const int tbl[16] =
+	static const int8_t tbl[16] =
 	{
 		 0, +1, -1,  0,		// position 3 = 00 to 11, can't really do anything, so 0
-		-1,  0, -2, +1,		// position 2 = 01 to 10, assume it was a bounce and should be 01 -> 00 -> 10
-		+1, +2,  0, -1,		// position 1 = 10 to 01, assume it was a bounce and should be 10 -> 00 -> 01
-		 0, -1, +1,  0		// position 0 = 11 to 00, can't really do anything
+		-1,  0,  0, +1,		// position 2 = 01 to 10, can't really do anything, so 0
+		+1,  0,  0, -1,		// position 1 = 10 to 01, can't really do anything, so 0
+		 0, -1, +1,  0		// position 0 = 11 to 00, can't really do anything, so 0
 	};
 
 	// Poll the encoder
 	const unsigned int t = ReadEncoderState();
-	const int movement = tbl[(encoderState << 2) | t];
+	const int8_t movement = tbl[(encoderState << 2) | t];
 	if (movement != 0)
 	{
-		encoderChange += movement;
+		encoderChange += (int)movement;
 		encoderState = t;
 	}
 
