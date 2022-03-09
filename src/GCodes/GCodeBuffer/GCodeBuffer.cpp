89c14792ff410f27fee31dbc08ab65563e0fd0db
Christian Hammacher <bmasterc@gmail.com>
2022-03-09 22:11:44 +0100

    Version 3.4-rc2+3
    Bug fix: Message acknowledgment wasn't always working as intended

--- src/GCodes/GCodeBuffer/GCodeBuffer.cpp
+++ src/GCodes/GCodeBuffer/GCodeBuffer.cpp
@@ -1016,7 +1016,7 @@ void GCodeBuffer::MessageAcknowledged(bool cancelled) noexcept
 			ms->messageAcknowledged = true;
 			ms->messageCancelled = cancelled;
 #if HAS_SBC_INTERFACE
-			messageAcknowledged = true;
+			messageAcknowledged = !cancelled || !ms->DoingFile();
 			reprap.GetSbcInterface().EventOccurred();
 #endif
 		}
