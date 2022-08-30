5d0c6ce9ee169a9371d06ef1bbe73c678314a846
David Crocker <dcrocker@eschertech.com>
2022-08-30 13:28:17 +0100

    Fixes for M917 on MB6XD

--- src/Platform/Platform.cpp
+++ src/Platform/Platform.cpp
@@ -659,7 +659,7 @@ void Platform::Init() noexcept
 		driveDriverBits[drive] = 0;
 		motorCurrents[drive] = 0.0;
 		motorCurrentFraction[drive] = 1.0;
-#if HAS_SMART_DRIVERS
+#if HAS_SMART_DRIVERS || SUPPORT_CAN_EXPANSION
 		standstillCurrentPercent[drive] = DefaultStandstillCurrentPercent;
 #endif
 		microstepping[drive] = 16 | 0x8000;						// x16 with interpolation
@@ -2848,7 +2848,7 @@ int Platform::GetMotorCurrent(size_t drive, int code) const noexcept
 		rslt = motorCurrentFraction[drive] * 100.0;
 		break;
 
-#if HAS_SMART_DRIVERS
+#if HAS_SMART_DRIVERS || SUPPORT_CAN_EXPANSION
 	case 917:
 		rslt = standstillCurrentPercent[drive];
 		break;
