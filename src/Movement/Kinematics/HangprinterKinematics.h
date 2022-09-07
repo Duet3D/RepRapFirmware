1664f517f7e38b556b3249198745eb0f0530bff8
Jorge Timón <jtimon@jtimon.cc>
2022-09-07 13:14:02 +0100

    UNTESTED: Hangprinter: Allow Hangprinter to be configured with 9 anchors

--- src/Movement/Kinematics/HangprinterKinematics.h
+++ src/Movement/Kinematics/HangprinterKinematics.h
@@ -52,8 +52,8 @@ protected:
 
 private:
 	// Basic facts about movement system
-	const char* ANCHOR_CHARS = "ABCDEFHI"; // Skip the G, since it is reserved in G-code
-	static constexpr size_t HANGPRINTER_MAX_AXES = 8;
+	const char* ANCHOR_CHARS = "ABCDEFHIJ"; // Skip the G, since it is reserved in G-code
+	static constexpr size_t HANGPRINTER_MAX_AXES = 9;
 	static constexpr size_t A_AXIS = 0;
 	static constexpr size_t B_AXIS = 1;
 	static constexpr size_t C_AXIS = 2;
