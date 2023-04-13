ae8d6698bd9915b89c9590daadec492b5c9bd7c0
David Crocker <dcrocker@eschertech.com>
2023-04-13 15:46:26 +0100

    Fix for M28 with indented lines

--- src/GCodes/GCodeBuffer/StringParser.cpp
+++ src/GCodes/GCodeBuffer/StringParser.cpp
@@ -1767,6 +1767,12 @@ void StringParser::WriteToFile() noexcept
 		}
 	}
 
+	size_t indent = commandIndent;
+	while (indent != 0)
+	{
+		fileBeingWritten->Write(' ');
+		--indent;
+	}
 	fileBeingWritten->Write(gb.buffer);
 	fileBeingWritten->Write('\n');
 	Init();
