ad9c82b5d377897a0bd91fb5e18938bb8b621a40
Christian Hammacher <chr.hammacher@gmail.com>
2023-08-31 10:33:34 +0200

    Bug fix for line number handling
    Line number was off when a while loop was repeated

--- src/GCodes/GCodeBuffer/StringParser.cpp
+++ src/GCodes/GCodeBuffer/StringParser.cpp
@@ -624,7 +624,7 @@ void StringParser::ProcessWhileCommand() THROWS(GCodeException)
 	}
 	else
 	{
-		gb.GetBlockState().SetLoopBlock(GetFilePosition(), gb.GetLineNumber());
+		gb.GetBlockState().SetLoopBlock(GetFilePosition(), gb.GetLineNumber() - 1);
 	}
 
 	if (!EvaluateCondition())
