b62d22577fcb223de8398b7fa0e004838aa90498
David Crocker <dcrocker@eschertech.com>
2024-03-26 17:27:29 +0000

    Fixed issue with Neopixel driver on Duet 2

--- src/LedStrips/NeoPixelLedStrip.h
+++ src/LedStrips/NeoPixelLedStrip.h
@@ -32,10 +32,10 @@ private:
 	static constexpr uint32_t DefaultNeoPixelSpiClockFrequency = 2500000;				// must be between about 2MHz and about 4MHz
 	static constexpr uint32_t MinNeoPixelResetTicks = (250 * StepClockRate)/1000000;	// 250us minimum Neopixel reset time on later chips
 
-	GCodeResult BitBangNeoPixelData(const LedParams& params) noexcept;
+	GCodeResult BitBangData(const LedParams& params) noexcept;
 
 #if SUPPORT_DMA_NEOPIXEL
-	GCodeResult SpiSendNeoPixelData(const LedParams& params) noexcept;
+	GCodeResult SpiSendData(const LedParams& params) noexcept;
 #endif
 
 	unsigned int numAlreadyInBuffer = 0;												// number of pixels already store in the buffer
