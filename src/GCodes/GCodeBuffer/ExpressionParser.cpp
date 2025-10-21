3b2d09bf72d3d5b1a3409788e7f2a0b8ec771c2f
David Crocker <dcrocker@eschertech.com>
2025-10-21 17:06:39 +0100

    Addendum to previous commit

--- src/GCodes/GCodeBuffer/ExpressionParser.cpp
+++ src/GCodes/GCodeBuffer/ExpressionParser.cpp
@@ -2247,7 +2247,7 @@ void ExpressionParser::GetVariableValue(ExpressionValue& rslt, const VariableSet
 							rslt.SetBool(false);
 							return;
 						}
-						ThrowParseException("Index out of range");
+						ThrowParseException(ArrayIndexOutOfRangeText);
 					}
 				}
 
