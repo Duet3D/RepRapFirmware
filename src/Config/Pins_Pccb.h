ef68530ddd4b0453b9351dea202ae17bb9811a79
Christian Hammacher <chr.hammacher@gmail.com>
2026-02-17 20:28:49 +0100

    Bug fixes
    Fixed compilation error for Duet 2 SBC build
    Fixed PCCB build config

--- src/Config/Pins_Pccb.h
+++ src/Config/Pins_Pccb.h
@@ -125,6 +125,9 @@ constexpr size_t MaxSpindles = 2;					// Maximum number of configurable spindles
 constexpr size_t MaxLedStrips = 2;					// Maximum number of LED strips
 
 constexpr size_t NumSerialChannels = 1;				// The number of serial IO channels (USB only)
+constexpr size_t FirstAuxChannel = 1;
+constexpr size_t NumAuxChannels = NumSerialChannels - FirstAuxChannel;
+
 #define SERIAL_USB_DEVICE serialUSB
 
 // SerialUSB
