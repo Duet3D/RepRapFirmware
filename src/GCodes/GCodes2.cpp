81d2089a824357182af56e2e6f221c62fec14cca
dc42 <dcrocker@duet3d.com>
2026-03-27 14:32:58 +0000

    Added build configuration for INDX board

--- src/GCodes/GCodes2.cpp
+++ src/GCodes/GCodes2.cpp
@@ -1964,7 +1964,7 @@ bool GCodes::HandleMcode(GCodeBuffer& gb, const StringRef& reply) THROWS(GCodeEx
 						reply.catf(" + %s", additionalExpansionName);
 					}
 				}
-#elif defined(DUET3MINI) && !defined(FMDC_V03)
+#elif defined(DUET3MINI) && !defined(FMDC_V03) && !defined(INDX)
 				{
 					const char *const expansionString = platform.GetExpansionBoardName();
 					if (expansionString != nullptr)
