a240ba1f0355816b6cd50a53b1638df39caa99aa
Christian Hammacher <chr.hammacher@gmail.com>
2026-07-21 09:58:59 +0200

    Reintroduced move aborts on axis limit violations
    A previous merge from 3.6 resurrected the axis limit behavior, now fixed again

--- src/GCodes/GCodes.cpp
+++ src/GCodes/GCodes.cpp
@@ -2607,16 +2607,8 @@ bool GCodes::DoStraightMove(GCodeBuffer& gb, bool isCoordinated) THROWS(GCodeExc
 			{
 			case LimitPositionResult::adjusted:
 			case LimitPositionResult::adjustedAndIntermediateUnreachable:
-				if (machineType != MachineType::fff)
-				{
-					abandonMove();
-					gb.ThrowGCodeException(TargetUnreachableText);				// it's a laser or CNC so this is a definite error
-				}
-				ToolOffsetInverseTransform(ms);									// make sure the limits are reflected in the user position
-				if (lp == LimitPositionResult::adjusted)
-				{
-					break;														// we can reach the intermediate positions, so nothing more to do
-				}
+				abandonMove();
+				gb.ThrowGCodeException(TargetUnreachableText);
 				[[fallthrough]];
 
 			case LimitPositionResult::intermediateUnreachable:
